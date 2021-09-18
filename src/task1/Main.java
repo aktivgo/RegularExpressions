7a02c3f97fa2c8f0233862da335d3fa380b01707
aktivgo <AktivGo01062001@gmail.com>
2021-09-18 23:35:41 +0300

    Add example by GeeksForGeeks

--- src/task1/Main.java
+++ src/task1/Main.java
@@ -1,8 +1,21 @@
 package task1;
 
+import java.util.regex.*;
+
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Test");
+        String regex = "\\b(?<Geeks>[A-Za-z\\s]+)";
+        Pattern pattern = Pattern.compile(regex);
+
+        String stringToBeMatched = "GeeksForGeeks";
+
+        Matcher matcher = pattern.matcher(stringToBeMatched);
+        MatchResult result = matcher.toMatchResult();
+
+        System.out.println("Current Matcher: " + result);
+        while (matcher.find()) {
+            System.out.println(matcher.start("Geeks"));
+        }
     }
 }
