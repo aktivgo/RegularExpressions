740801f8ead15a52fcaa788a621619e5d8ca85a2
aktivgo <AktivGo01062001@gmail.com>
2021-09-21 18:10:26 +0300

    Complete Ex2

--- src/Task/IsPattern.java
+++ src/Task/IsPattern.java
@@ -4,23 +4,27 @@ import java.util.regex.Pattern;
 
 public class IsPattern {
 
-    static boolean IP(String str){
-        String ipPattern = "^(([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])\\.){3}([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])$";
+    static boolean IPv4(String str){
+        String ipPattern =
+                "^(([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])\\.){3}([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])$";
         return Pattern.matches(ipPattern, str);
     }
 
     static boolean GUID(String str){
-        String guidPattern = "";
+        String guidPattern =
+                "(^\\{[A-Fa-f0-9]{8}-([A-Fa-f0-9]{4}-){3}[A-Fa-f0-9]{12}\\}$)|(^[A-Fa-f0-9]{8}-([A-Fa-f0-9]{4}-){3}[A-Fa-f0-9]{12}$)";
         return Pattern.matches(guidPattern, str);
     }
 
     static boolean URL(String str){
-        String urlPattern = "";
+        String urlPattern =
+                "";
         return Pattern.matches(urlPattern, str);
     }
 
     static boolean Password(String str){
-        String passwordPattern = "";
+        String passwordPattern =
+                "";
         return Pattern.matches(passwordPattern, str);
     }
 }
--- src/Task/Main.java
+++ src/Task/Main.java
@@ -7,29 +7,33 @@ public class Main {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
         do {
-            System.out.print("1. Проверка корректности IP адреса\n2. Проверка корректности GUID идентификатора\n" +
+            System.out.print("1. Проверка корректности IPv4 адреса\n2. Проверка корректности GUID идентификатора\n" +
                     "3. Проверка корректности URL адреса\n4. Проверка надёжности пароля\n5. Выход\n\nВыберите пункт: ");
             try {
                 int menu = Integer.parseInt(scan.nextLine());
                 switch (menu) {
                     case 1: {
-                        System.out.print("\nВведите IP-адрес: ");
-                        System.out.println(IsPattern.IP(new Scanner(System.in).nextLine()) ? "IP-адрес корректный\n" : "IP-адрес некорректный\n");
+                        System.out.print("\nВведите IPv4-адрес: ");
+                        System.out.println(IsPattern.IPv4(new Scanner(System.in).nextLine()) ?
+                                "IPv4 адрес корректный\n" : "IPv4 адрес некорректный\n");
                     }
                     break;
                     case 2: {
                         System.out.print("\nВведите GUID идентификатор: ");
-                        System.out.println(IsPattern.IP(new Scanner(System.in).nextLine()) ? "IP-адрес корректный\n" : "IP-адрес некорректный\n");
+                        System.out.println(IsPattern.GUID(new Scanner(System.in).nextLine()) ?
+                                "GUID идентификатор корректный\n" : "GUID идентификатор некорректный\n");
                     }
                     break;
                     case 3: {
                         System.out.print("\nВведите URL адрес: ");
-                        System.out.println(IsPattern.IP(new Scanner(System.in).nextLine()) ? "IP-адрес корректный\n" : "IP-адрес некорректный\n");
+                        System.out.println(IsPattern.URL(new Scanner(System.in).nextLine()) ?
+                                "URL адрес корректный\n" : "URL адрес некорректный\n");
                     }
                     break;
                     case 4: {
                         System.out.print("\nВведите пароль: ");
-                        System.out.println(IsPattern.IP(new Scanner(System.in).nextLine()) ? "IP-адрес корректный\n" : "IP-адрес некорректный\n");
+                        System.out.println(IsPattern.Password(new Scanner(System.in).nextLine()) ?
+                                "Пароль надёжный\n" : "Пароль ненадёжный\n");
                     }
                     break;
                     case 5: return;
